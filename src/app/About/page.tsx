0db8490e492c2765551ffb54be9f99f8392c8606
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2024-11-10 23:08:59 +0500

    page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -10,12 +10,12 @@ export default function About() {
         <div className="skills">
           <h3 className="skills-title">My Skills</h3>
           <div className="skill-bars">
-            <SkillBar skill="HTML" level={90%} />
-            <SkillBar skill="CSS" level={85%} />
-            <SkillBar skill="JavaScript" level={80%} />
-            <SkillBar skill="TypeScript" level={75%} />
-            <SkillBar skill="Next.js" level={70%} />
-            <SkillBar skill="Tailwind CSS" level={80%} />
+            <SkillBar skill="HTML" level={90} />
+            <SkillBar skill="CSS" level={85} />
+            <SkillBar skill="JavaScript" level={80} />
+            <SkillBar skill="TypeScript" level={75} />
+            <SkillBar skill="Next.js" level={70} />
+            <SkillBar skill="Tailwind CSS" level={80} />
           </div>
         </div>
       </div>
