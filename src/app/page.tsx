a3a929cf28ce07886d5e40078486f9bed52e644f
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2024-11-24 01:09:38 +0500

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,25 @@ import { FaFacebook } from "react-icons/fa";
 import { FaTwitterSquare } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6"; 
 
-import { PiX } from 'react-icons/pi';
-
 export default function Home() {
   return (
-    <section className='home'>
-    <div className='home-content'>
-      <h1>Hi, I am Azka Khizer</h1>
-      <h2>Frontend Developer</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ad accusamus unde earum quas expedita obcaecati non distinctio, deleniti aliquid maxime ipsum? Placeat optio libero ea fuga?</p>
-      <div className="btn-box">
-        <a href='/Contact'>Hire me</a>
-        <a href="/Contact">Let's Talk</a>
+    <section className="home">
+      <div className="home-content">
+        <h1>Hi, I am Azka Khizer</h1>
+        <h2>Frontend Developer</h2>
+        <p>I am Azka Khizer, a Frontend Developer focused on building responsive and user-friendly websites. I am always learning and improving my skills to create seamless, high-quality web experiences.</p>
+        <div className="btn-box">
+          <a href="/Contact">Hire me</a>
+          <a href="/Contact">Let Us Talk</a>
+        </div>
       </div>
-    </div>
-    <div className='icons'>
-    <a href="https://www.facebook.com/azka.khizer"><FaFacebook /></a>
-    <a href="#"><FaTwitterSquare /></a>
-    <a href="https://www.instagram.com/marketminds123/"><FaInstagram /></a>
-    
-    </div>
-    <span className='home-imghover'></span>
+      <div className="icons">
+        <a href="https://www.facebook.com/azka.khizer" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+        <a href="#" target="_blank" rel="noopener noreferrer"><FaTwitterSquare /></a>
+        <a href="https://www.instagram.com/marketminds123/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+      </div>
+      <span className="home-imghover"></span>
     </section>
-
-
-
   );
 }
+
